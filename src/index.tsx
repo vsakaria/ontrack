a3fc4318c655abe44d0adedf55c986d17160a636
Vishal Sakaria <vsakaria@gmail.com>
2021-03-01 19:14:32 +0000

    import bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./index.css";
+import "./themes/brand.scss";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
