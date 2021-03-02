5fec4e26eb49b4643fe634dbda75d2db834fc898
Vishal Sakaria <vsakaria@gmail.com>
2021-03-02 08:29:52 +0000

    UPDATE styling and ADD disable next

--- src/components/BookList/index.tsx
+++ src/components/BookList/index.tsx
@@ -3,9 +3,12 @@ import api from "../../api"
 import { Container, Pagination, Row } from 'react-bootstrap'
 import { BookCard } from "../BookCard";
 import { SearchField } from "../SearchField";
+import { Col, Jumbotron } from "reactstrap";
 
 const BooksList = ({ initPage }: { initPage: number }): ReactElement => {
   const [books, setBooks] = useState([])
+  const [count, setCount] = useState(0)
+
   const [page, setPage] = useState(initPage)
   const [search, setSearch] = useState("")
 
@@ -15,8 +18,9 @@ const BooksList = ({ initPage }: { initPage: number }): ReactElement => {
       itemsPerPage: 20,
       filters: [{ type: "all", values: [search] }]
     })
-      .then(({ books }) => {
+      .then(({ books, count }) => {
         setBooks(books)
+        setCount(count)
       })
   }, [page, search])
 
@@ -31,26 +35,33 @@ const BooksList = ({ initPage }: { initPage: number }): ReactElement => {
 
   return (
     <Container>
-
-      <Row>
-        <h1 className="bg-primary ">On track book app</h1>
-      </Row>
-
-      <Row>
-        <SearchField handleSubmit={handleSubmit} />
-      </Row>
-
-      <Row>
-        {books.map((book: any) => {
-          return <BookCard key={book.id} book={book} />
-        })}
-      </Row>
-      <Row>
-        <Pagination>
-          <Pagination.Prev disabled={page === 1} onClick={(): void => { setPage(page - 1) }}> Previous </Pagination.Prev>
-          <Pagination.Next data-testid="next" onClick={(): void => { setPage(page + 1) }}> Next </Pagination.Next>
-        </Pagination>
-      </Row>
+      <Col xs="12" sm="12">
+        <Row>
+          <Jumbotron className="mt-4 mb-3 bg-primary text-centre text-white">
+            <h1>On track book app</h1>
+          </Jumbotron>
+        </Row>
+      </Col>
+      <Col xs="12" sm="6">
+        <Row>
+          <SearchField handleSubmit={handleSubmit} />
+        </Row>
+      </Col>
+      <Col xs="12" sm="12">
+        <Row>
+          {books.map((book: any) => {
+            return <BookCard key={book.id} book={book} />
+          })}
+        </Row>
+      </Col>
+      <Col xs="12" sm="6">
+        <Row>
+          <Pagination>
+            <Pagination.Prev disabled={page === 1} onClick={(): void => { setPage(page - 1) }}> Previous </Pagination.Prev>
+            <Pagination.Next disabled={page * 20 >= count} data-testid="next" onClick={(): void => { setPage(page + 1) }}> Next </Pagination.Next>
+          </Pagination>
+        </Row>
+      </Col>
 
     </Container >
   );
